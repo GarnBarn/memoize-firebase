6dc301f5a18a7f26fdeb8e963d2b1a270a90b20f
Siratee K <siratee.k@lmwn.com>
2024-04-15 16:36:02 +0700

    feat: Add Ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,14 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+import {onSchedule} from "firebase-functions/v2/scheduler";
 import * as logger from "firebase-functions/logger";
+import { initializeApp } from "firebase-admin/app";
+import { credential, firestore } from "firebase-admin";
+import { getFirestore } from "firebase-admin/firestore";
+import { onRequest } from "firebase-functions/v2/https";
+import * as sa from "./key.json";
+
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +23,35 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const manualTrigger = onRequest((req, res) => {
+    coreLogic()
+    res.send("Done!");
+})
+
+
+export const ScheduledNotifier = onSchedule("every minutes", async (event) => {
+    coreLogic()
+}) 
+
+
+const coreLogic =  async () => {
+    logger.info("Starting to check the notifier task")
+
+
+    const app =initializeApp({
+        credential: credential.cert(sa)
+    })
+    
+   const db  = getFirestore(app)
+
+   let focusedDate = new Date(new Date().getTime() - 30 * 60000)
+
+    const result = await db.collection("reminder").where("reminder_date_time", "==", focusedDate).get()
+
+    if (result.docs.length == 0) {
+        logger.info("No Document to perform, skipping")
+    }
+
+    logger.info("Notifying ", result.docs.length, " reminder(s)")
+}
