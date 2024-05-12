4991e4ae2fa007256b845a8cef672344ff84f43d
Siratee K <siratee.k@lmwn.com>
2024-05-12 15:34:45 +0700

    feat: Revise the notification

--- functions/index.js
+++ functions/index.js
@@ -27,8 +27,8 @@ exports.testFunc = onRequest((request, response) => {
     response.send("Done Processing!");
 });
 
-exports.memoizeNotifier = onSchedule("every minute", () => {
-    coreLogic()
+exports.memoizeNotifier = onSchedule("every minute", async () => {
+    await coreLogic()
     logger.info("Done Processing");
 });
 
@@ -36,12 +36,19 @@ const coreLogic =  async () => {
     logger.info("Starting to check the notifier task")
 
    const db  = app.firestore()
+   let d = new Date(); 
+   d = new Date(d.toUTCString()).setSeconds(0)
+   logger.info("UTC: ", d.toLocaleString("en-US"))
 
-   let focusedDate = new Date(new Date().getTime() + 30 * 60000)
-   let upperDate = new Date(new Date().getTime() + 31 * 60000)
+   let focusedDate = new Date(new Date(d  + (30 * 60000)).toLocaleString("en-US"))
+   let upperDate = new Date(new Date(d + (31 * 60000)).toLocaleString("en-US"))
+
+   logger.info("Checking time between: ", focusedDate)
+   logger.info("To: ", upperDate)
 
     const result = await db.collection("reminder").where("reminder_date_time", ">=", focusedDate).where("reminder_date_time", "<", upperDate).get()
 
+    console.log(result.docs)
     if (result.docs.length == 0) {
         logger.info("No Document to perform, skipping")
         return
@@ -61,16 +68,10 @@ const coreLogic =  async () => {
         const fcmToken = users[uid]
         
         const message = {
-            notification: { title: `Reminder [${data["title"]}] due in 30 mins`, body: 'Check it before it due!' },
+            notification: { title: `Reminder [${data["title"]} from ${data["reminder_set"]}] due in 30 mins`, body: 'Check it before it due!' },
             token: fcmToken
           };
         const result2 = await app.messaging().send(message)
         logger.info(result2)
     }
-
-    
-
-    // Get all user
-  
-
 }
